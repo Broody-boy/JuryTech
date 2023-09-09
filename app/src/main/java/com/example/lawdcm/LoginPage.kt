226b626bfa7530e8c2895d720a9d4a111a4b343f
madhav <guptamadhav137@gmail.com>
2023-09-10 00:31:40 +0530

    Added Login Page Functionality And Added DataClass for Registrar

--- app/src/main/java/com/example/lawdcm/LoginPage.kt
+++ app/src/main/java/com/example/lawdcm/LoginPage.kt
@@ -1,19 +1,32 @@
 package com.example.lawdcm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import com.example.lawdcm.databinding.ActivityLoginPageBinding
+import com.example.lawdcm.models.RegistrarAuth
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginPage : AppCompatActivity() {
     private lateinit var binding: ActivityLoginPageBinding
     private lateinit var auth: FirebaseAuth
-    private lateinit var database: DatabaseReference
+    val dbreference = FirebaseDatabase.getInstance().getReference("registrars")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
-        database = FirebaseDatabase.getInstance().getReference("registrars")
+
+
 
         if(auth.currentUser != null) {
             startActivity(Intent(this, MainActivity::class.java))
@@ -25,6 +38,36 @@ class LoginPage : AppCompatActivity() {
         val passKey = binding.etPassKEY;
 
         binding.btnLogin.setOnClickListener {
+
+            val courtType = findViewById<RadioButton>(binding.rgCourtType.checkedRadioButtonId).text.toString().uppercase()
+
+            val dbquery = dbreference.child(binding.etRegistrarId.text.toString())
+            dbquery.addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()){
+
+                        val data = snapshot.getValue(RegistrarAuth::class.java)
+
+                        if (data != null) {
+                            if(binding.etEmail.text.toString() == data.officialEmail && binding.etPassKEY.text.toString() == data.passKey
+                                && courtType == data.courtType){
+                                startActivity(Intent(this@LoginPage , MainActivity::class.java))
+                                finish()
+                            }
+                            else{
+                                Toast.makeText(this@LoginPage, "Wrong Credentials", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }else{
+                        Toast.makeText(this@LoginPage, "Registrar Id not valid", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
 
         }
     }
