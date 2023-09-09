1bf58f006027c974129c09f17c065b92538f096b
madhav <guptamadhav137@gmail.com>
2023-09-09 23:37:19 +0530

    Login Base Setup And ViewBinding is added

--- app/src/main/java/com/example/lawdcm/LoginPage.kt
+++ app/src/main/java/com/example/lawdcm/LoginPage.kt
@@ -4,8 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class LoginPage : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginPageBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_page)
+        binding = ActivityLoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().getReference("registrars")
+
+        if(auth.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        val registrarId = binding.etRegistrarId;
+        val officialEmail = binding.etEmail;
+        val passKey = binding.etPassKEY;
+
+        binding.btnLogin.setOnClickListener {
+
+        }
     }
 }
