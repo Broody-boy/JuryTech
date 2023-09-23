a8c5e400348f4771512d597999b704283a2d632e
Rishabh <rarishabhjain@gmail.com>
2023-09-23 15:43:50 +0530

    Added spinners for caseCategory and caseType

--- app/src/main/java/com/example/lawdcm/singleton/ActiveJudges.kt
+++ app/src/main/java/com/example/lawdcm/singleton/ActiveJudges.kt
@@ -1,23 +1,11 @@
 package com.example.lawdcm.singleton
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.lawdcm.models.JudgeDetails
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 object ActiveJudges {
-
     init {
 
     }
-
     var activeJudgesList = MutableLiveData<ArrayList<JudgeDetails>>()
-
-
-
-
-
 }
